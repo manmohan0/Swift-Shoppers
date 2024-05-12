68e651e88bb8a8e22f944c0e58a608f0a7a2b015
Manmohan Wable <manmohanwable0@gmail.com>
2024-05-12 12:12:49 +0530

    tailwind css working but image and input box are not able to align horizontally

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -1,17 +1,15 @@
 export const Appbar = () => {
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-            <div className="flex flex-row">
-                <div>
-                    <img src="https://firebasestorage.googleapis.com/v0/b/swiftshopper01.appspot.com/o/Swift%20Shopper%20Logo.jpg?alt=media&token=58113a66-4063-4211-8292-6a6bb218214e" alt="Swift Shoppers Logo" width={'100px'} height={'40px'}/>
-                </div>
-                <div className="border border-slate-400 px-3 py-3 m-2">
+        <nav>
+            <div className="flex flex-row ">
+                <span>
+                    <img className="p-0 m-0" src="https://firebasestorage.googleapis.com/v0/b/swiftshopper01.appspot.com/o/Swift%20Shopper%20Logo.jpg?alt=media&token=58113a66-4063-4211-8292-6a6bb218214e" alt="Swift Shoppers Logo" width={'65px'} height={'40px'}/>
+                </span>
+                <span className="border border-slate-400 px-3 py-3">
                     <input className="p-3" type="text" name="search" id="search" />
-                </div>
+                </span>
+                
             </div>
-        </div>
+        </nav>
     );
 };
