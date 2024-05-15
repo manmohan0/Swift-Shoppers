228a93433d896917c075502f5f651b50b1aee07a
Manmohan Wable <manmohanwable0@gmail.com>
2024-05-15 13:15:40 +0530

    Changed title and description, Finally fixed Tailwind CSS warning, Completed Appbar

--- apps/Store/tailwind.config.js
+++ apps/Store/tailwind.config.js
@@ -3,7 +3,9 @@ module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",    
+    "../../packages/ui/**/*{.js,.ts,.jsx,.tsx}",
+
  
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -1,9 +1,29 @@
 export const Appbar = () => {
     return (
-    <nav className="w-full">
+    <nav className="w-full shadow">
         <div className="flex flex-row align-middle w-full h-auto">
-            <img className="w-14 m-0 p-0" src="https://firebasestorage.googleapis.com/v0/b/swiftshopper01.appspot.com/o/Swift-Shopper-Logo.svg?alt=media&token=5d908f82-e364-4ad1-8cf5-4c0977d4bb16" alt="Swift Shoppers Logo"/>
-            <input className="p-0 m-2 ml-40 rounded-md flex-auto" type="text" name="search" id="search" />
+            <a href="/">
+                <img className="w-16 ml-20 mr-20 my-0 rounded-md" src="https://firebasestorage.googleapis.com/v0/b/swiftshopper01.appspot.com/o/Swift-Shopper-Logo.svg?alt=media&token=5d908f82-e364-4ad1-8cf5-4c0977d4bb16" alt="Swift Shoppers Logo"/>
+            </a>
+            <input className="p-0 px-3 m-2 ml-20 bg-slate-100 rounded-md border-gray-300 flex-auto" placeholder="Search for Products, Brands and More" type="text" name="search" id="search" />
+            <div className="p-3 pl-20 pr-0">
+
+                <a className="px-6 py-4 hover:bg-slate-100 cursor-pointer" href="#">
+                    <span>
+                        Login
+                    </span>
+                </a>
+                <a className="px-6 py-4 hover:bg-slate-100 cursor-pointer" href="#">
+                    <span>
+                        Cart
+                    </span>
+                </a>
+                <a className="px-6 py-4 hover:bg-slate-100 cursor-pointer" href="#">
+                    <span className="">
+                        Become a Seller
+                    </span>
+                </a>
+            </div>
         </div>
     </nav>
     );
